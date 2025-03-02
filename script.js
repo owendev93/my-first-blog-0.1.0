3f8a76b41b16966d2d263de425d7ae789238d4a7
Owen Puerta Sauto <owenpuertasauto@gmail.com>
2025-03-02 14:24:05 -0300

    update all project

--- script.js
+++ script.js
@@ -4,4 +4,19 @@ const container = document.querySelector(".container");
 menuIcon.addEventListener("click",()=>{
     container.classList.toggle("change");
 });
-  
+
+
+var swiper = new Swiper (".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  coverflowEffect: {
+      depth: 500,
+      modifer: 1,
+      sliderShadows: true,
+      rotate: 0,
+      stretch: 0
+  }
+});
