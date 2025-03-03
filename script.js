00f72cacc7d10ffd782bf8cc7e5c62bdf5ed33e6
Owen Puerta Sauto <owenpuertasauto@gmail.com>
2025-03-03 14:33:43 -0300

    Update all project

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const container = document.querySelector(".container");
 menuIcon.addEventListener("click",()=>{
     container.classList.toggle("change");
 });
+
+
+
+//descarga de CV
+document.getElementById("downloadcv").addEventListener("click", function() {
+    let link = document.createElement("a");
+    link.href = "src/docs/cv-ingdev.pdf"; // Ruta del archivo
+    link.download = "cv-ingdev.pdf"; // Nombre del archivo al descargar
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
