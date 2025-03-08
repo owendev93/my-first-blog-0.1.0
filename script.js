51f13fd65415b61db15cc09932ebfb0ffd13b30d
Owen Puerta Sauto <owenpuertasauto@gmail.com>
2025-03-08 13:22:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function scrollUp(){
 buttonUp = document.getElementById("button-up");
 window.onscroll = function(){
     var scroll = document.documentElement.scrollTop;
-    if(scroll > 500){
+    if(scroll > 200){
         buttonUp.style.transform = "scale(1)";
     }else if(scroll < 500){
         buttonUp.style.transform = "scale(0)";
