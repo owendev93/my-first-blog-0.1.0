7ad5d8b6f4f337de421a1d02a29012631ad61872
owendev93 <owenpuertasauto@gmail.com>
2025-04-13 14:11:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -80,21 +80,21 @@ let notification = document.querySelector('.notification');
 function createToast(type, icon, title, text) {
     let newToast = document.createElement('div');
     newToast.innerHTML = `
-      <div class="toast ${type}">
-              <i class="${icon}"></i>
-              <div class="content">
-                  <div class="title">${title}</div>
-                  <span>${text}</span>
-              </div>
-              <i class="fa-solid fa-xmark"
-              onclick="(this.parentElement).remove()"
-              ></i>
-          </div>`;
-      notification.appendChild(newToast);
-      newToast.timeOut = setTimeout(
-          () => newToast.remove(), 5000
-      );
-  }
+    <div class="toast ${type}">
+            <i class="${icon}"></i>
+            <div class="content">
+                <div class="title">${title}</div>
+                <span>${text}</span>
+            </div>
+            <i class="fa-solid fa-xmark"
+            onclick="(this.parentElement).remove()"
+            ></i>
+        </div>`;
+    notification.appendChild(newToast);
+    newToast.timeOut = setTimeout(
+        () => newToast.remove(), 5000
+    );
+}
 
 document.getElementById('form')
 .addEventListener('submit', function(event) {
